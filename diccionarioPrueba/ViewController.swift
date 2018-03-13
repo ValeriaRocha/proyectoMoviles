69a60cce306f8667a92e581bb0a7d4c5ecee5a12
YOUR NAME <YOUR@EMAIL.com>
2018-03-12 21:03:10 -0600

    Initial Commit

---
+++ diccionarioPrueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  diccionarioPrueba
+//
+//  Created by Valeria Rocha Sepulveda  on 12/03/18.
+//  Copyright © 2018 Valeria Rocha Sepulveda . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
