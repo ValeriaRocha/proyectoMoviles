93e85c6e64a9e471e790a8d46ef5c536d1717835
Mickey Rocha <mickeyrocha@Mickeys-MacBook-Air.local>
2018-04-03 10:01:56 -0500

    se agrego el juego memorama, todavia falta terminarlo y falta checar los identifiers de las cartas

---
+++ diccionarioPrueba/MemoramaCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemoramaCollectionViewCell.swift
+//  diccionarioPrueba
+//
+//  Created by Mickey Rocha on 4/2/18.
+//  Copyright © 2018 Valeria Rocha Sepulveda . All rights reserved.
+//
+
+import UIKit
+
+class MemoramaCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
